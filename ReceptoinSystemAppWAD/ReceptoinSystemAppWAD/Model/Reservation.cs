23c222b223883c2f6c37056ec75a6db2f9fc4a20
00013372wiut <00013372@wiut.uz>
2024-03-16 14:51:40 +0500

    Guest Repository created

---
+++ ReceptoinSystemAppWAD/ReceptoinSystemAppWAD/Model/Reservation.cs
@@ -0,0 +1,14 @@
+﻿namespace ReceptoinSystemAppWAD.Model
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+
+
+    }
+}
---
+++ ReceptoinSystemAppWAD/ReceptoinSystemAppWAD/Model/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace ReceptoinSystemAppWAD.Model
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomType { get; set; }
+        public string RoomStatus { get; set;}
+    }
+}
