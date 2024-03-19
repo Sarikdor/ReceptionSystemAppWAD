1c5015c6b8fd41844c86703964eb2e7e3122a5e8
00013372wiut <00013372@wiut.uz>
2024-03-19 12:50:57 +0500

    Cleaning

--- ReceptoinSystemAppWAD/ReceptoinSystemAppWAD/Model/Room.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ReceptoinSystemAppWAD.Model
-{
-    public class Room
-    {
-        public int RoomId { get; set; }
-        public string RoomType { get; set; }
-        public string RoomNum { get; set;}
-    }
-}
